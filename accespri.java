19262b3b30df3d1044050afd3135bd629f16fa6b
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-11 11:13:48 +0530

    Add files via upload

---
+++ accespri.java
@@ -0,0 +1,11 @@
+public class accespri {
+    private static void s()
+    {
+        System.out.println("Hello students");
+    }
+    public static void main(String[] args){
+        s();
+    }
+    
+}
+
