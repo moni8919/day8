dfbdbcd5c6aaa6a73788a75331b8d3f143740890
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-11 13:17:17 +0530

    Add files via upload

---
+++ staticwithnoaces.java
@@ -0,0 +1,14 @@
+public class staticwithnoaces {
+     static String s()
+    {
+        return"Hello students";
+    }
+    public static void main(String[] args){
+        String res=s();
+        System.out.println(res);
+    }
+    
+}
+ 
+    
+
