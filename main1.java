df72ca9636b121e02340d296564b790bc0ce600c
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-11 15:19:40 +0530

    Add files via upload

---
+++ main1.java
@@ -0,0 +1,15 @@
+public  class main1 {
+    void display()
+    {
+        System.out.println("Hello students");
+    } 
+    void dis1(){
+        this.display();
+    }
+    public static void main(String[] args){
+        main1  o=new main1();
+        o.dis1();
+    }
+
+    
+}
