991852447750404b0725f2b9f0d00e305a6bfde5
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-11 15:20:08 +0530

    Add files via upload

---
+++ construct.java
@@ -0,0 +1,24 @@
+ import java.util.*;
+      class a {
+    int c;
+    String b;
+    a()
+    {
+        this.c=345;
+        this.b="hello mounika";
+
+    }
+    void display()
+    {
+        System.out.println(b+" "+c);
+    }
+    }
+    public class construct{
+        public static void main(String[] args){
+            a s=new a();
+            s.display();
+        }
+
+    }
+ 
+
