8b33484c294fd5011dc0f2b2009145dc177bd872
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-11 11:15:23 +0530

    Add files via upload

---
+++ accespub.java
@@ -0,0 +1,10 @@
+public class accespub {
+    public static void s()
+    {
+        System.out.println("Hello students");
+    }
+    public static void main(String[] args){
+        s();
+    }
+    
+}
